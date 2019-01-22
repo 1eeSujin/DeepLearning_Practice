dbf80cad88ba644ef31d295069d3744bd0818d68
1eeSujin <bethy.sujin.lee@gmail.com>
2019-01-22 23:31:31 +0900

    Compute gradient and apply gradient Pratice using tensorflow

--- Linear_regression.py
+++ Linear_regression.py
@@ -1,25 +1,27 @@
 import tensorflow as tf
 tf.set_random_seed(777)  # for reproducibility
 
-# X and Y data
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
+
 
 W = tf.Variable(tf.random_normal([1]), name="weight")
 b = tf.Variable(tf.random_normal([1]), name="bias")
-hypothesis = x_train * W + b
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 
-# cost/loss function
-cost = tf.reduce_mean(tf.square(hypothesis - y_train))
-
-train = tf.train.GradientDescentOptimizer(learning_rate = 0.01).minimize(cost)
+hypothesis = X * W + b
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
 with tf.Session() as sess:
-    sess. run(tf.global_variables_initializer())
+    sess.run(tf.global_variables_initializer())
 
     for step in range(2001):
-        _, cost_val, W_val, b_val = sess.run([train,cost,W,b])
-
+        _, cost_val, W_val, b_val = sess.run(
+            [train, cost, W, b], feed_dict={X: [1, 2, 3], Y: [1, 2, 3]}
+        )
         if step % 20 == 0:
             print(step,cost_val,W_val,b_val)
 
+    print(sess.run(hypothesis,feed_dict={X:[5]}))
+    print(sess.run(hypothesis, feed_dict={X: [2.5]}))
+    print(sess.run(hypothesis, feed_dict={X: [1.5,3.5]}))
