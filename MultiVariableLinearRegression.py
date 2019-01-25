3ef2d336c58b6dfc2b3e6bae625f3c55ca00db91
1eeSujin <bethy.sujin.lee@gmail.com>
2019-01-25 19:48:59 +0900

    Multi variable linear regression using tensorflow

---
+++ MultiVariableLinearRegression.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+tf.set_random_seed(777)
+
+x1_data = [73., 93., 89., 96., 73.]
+x2_data = [80., 88., 91., 98., 66.]
+x3_data = [75., 93., 90., 100., 70.]
+
+# 우리가 목표로 하는 y 값
+# 이 코드를 실행하면, y값과 비슷하게 수렴
+y_data = [152., 185., 180., 196., 142.]
+
+# placeholders for a tensor
+x1 = tf.placeholder(tf.float32)
+x2 = tf.placeholder(tf.float32)
+x3 = tf.placeholder(tf.float32)
+
+Y = tf.placeholder(tf.float32)
+
+w1 = tf.Variable(tf.random_normal([1]), name='weight1')
+w2 = tf.Variable(tf.random_normal([1]), name='weight2')
+w3 = tf.Variable(tf.random_normal([1]), name='weight3')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
+
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
+                                   feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
+    if step % 10 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
